3a74144107b9a6767d61204e8a35e05abbbf5ab6
Charuth Sudhakaran <charuthsudhakaran@gmail.com>
2020-07-19 14:07:51 +0530

    First commit

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+  attr_accessor :password_required  # virtual attribute to skip password validation while saving
+
+  protected
+
+  def password_required?
+    false
+  end
+end
